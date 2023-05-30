d8243cd735fcfb359e57bc30ba9553c868b58269
ARAVIND KRISHNAN K <aravindkrishnankr@gmail.com>
2023-05-30 16:57:41 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "passguard-41094",
   url: "https://passguard-41094.botics.co",
   api: "https://passguard-41094.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
